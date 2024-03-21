1f03341aa1e30e4795347fd4d5edf4af6165efa3
gourav <gauravswain@gmail.com>
2024-03-21 16:37:26 +0000

    updated shop and memberszone

--- javascript/memberScript.js
+++ javascript/memberScript.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", async function(){
   // placeholder for future revisions of loadData() and renderList()
+  async function loadData(){
+    users = [];
+    users = await loadUserData();
+    renderList(users);
+  };
+  loadData();
 })
 
 // function for the add button
@@ -35,24 +41,25 @@ const addButton = document.querySelector("#addUser");
         addUser(users, userName, userEmail);
         renderList(users);
         alert(`Thank you ${userName} for registering your GetHealthy membership! Check your email inbox for the 10% offer coupon.`);
+        saveUserData(users);
         return;
       }
     }
   });
 
-// function for the load button
-const loadButton = document.querySelector("#load-btn");
-  loadButton.addEventListener("click", async function() {
-    users = [];
-    users = await loadUserData();
-    renderList(users);
-  });
+// // function for the load button
+// const loadButton = document.querySelector("#load-btn");
+//   loadButton.addEventListener("click", async function() {
+//     users = [];
+//     users = await loadUserData();
+//     renderList(users);
+//   });
 
-// function for the save button
-const saveButton = document.querySelector("#save-btn");
-  saveButton.addEventListener("click", async function() {
-    saveUserData(users);
-  });
+// // function for the save button
+// const saveButton = document.querySelector("#save-btn");
+//   saveButton.addEventListener("click", async function() {
+//     saveUserData(users);
+//   });
 
 // function for rendering the list
 function renderList(users){
@@ -62,45 +69,47 @@ function renderList(users){
   for (let u of users) {
     const listItem = document.createElement('li');
     listItem.className = "list-item";
-    listItem.innerHTML = `${u.name} (${u.email})
-      <button class="btn btn-primary btn-sm edit-btn">Edit</button>
-      <button class="btn btn-danger btn-sm delete-btn">Delete</button>`;
+    listItem.innerHTML = `${u.name}` 
+    // `(${u.email})
+    //   <button class="btn btn-primary btn-sm edit-btn">Edit</button>
+    //   <button class="btn btn-danger btn-sm delete-btn">Delete</button>`;
     
-    // function for the edit button
-    const editButton = listItem.querySelector(".edit-btn");
-    editButton.addEventListener("click", function(){
-      let newUserName = prompt("Please enter the new name");
-      let newUserEmail = prompt("Please enter the new email");
-      for (let u of users) {
-        if (newUserEmail == u.email) {
-          alert("Email is already registered, please use another email.");
-          return;
-        }
-      }
-      while (true) {
-        if (newUserName == null || newUserEmail == null) {
-          alert("Incorrect details entered, please try again.");
-          return;
-        }
-        else if (newUserName == "" || newUserEmail == "") {
-          alert("Details can't be blank, please try again.");
-          return;
-        }
-        else {
-          editUser(users, u.userid, newUserName, newUserEmail);
-          renderList(users);
-          alert("Edited Successfully.");
-          return;
-        }
-      }
-    });
+    // // function for the edit button
+    // const editButton = listItem.querySelector(".edit-btn");
+    // editButton.addEventListener("click", function(){
+    //   let newUserName = prompt("Please enter the new name");
+    //   let newUserEmail = prompt("Please enter the new email");
+    //   for (let u of users) {
+    //     if (newUserEmail == u.email) {
+    //       alert("Email is already registered, please use another email.");
+    //       return;
+    //     }
+    //   }
+    //   while (true) {
+    //     if (newUserName == null || newUserEmail == null) {
+    //       alert("Incorrect details entered, please try again.");
+    //       return;
+    //     }
+    //     else if (newUserName == "" || newUserEmail == "") {
+    //       alert("Details can't be blank, please try again.");
+    //       return;
+    //     }
+    //     else {
+    //       editUser(users, u.userid, newUserName, newUserEmail);
+    //       renderList(users);
+    //       alert("Edited Successfully.");
+    //       return;
+    //     }
+    //   }
+    // });
     
-    // function for the delete button
-    const deleteButton = listItem.querySelector(".delete-btn")
-      deleteButton.addEventListener("click", function(){
-        deleteUser(users, u.userid);
-        renderList(users);
-      });
+    // // function for the delete button
+    // const deleteButton = listItem.querySelector(".delete-btn")
+    //   deleteButton.addEventListener("click", function(){
+    //     deleteUser(users, u.userid);
+    //     renderList(users);
+    //   });
+
     // call for adding to the list
     userListElement.appendChild(listItem);  
   }
--- javascript/shopScript.js
+++ javascript/shopScript.js
@@ -7,4 +7,16 @@ document.addEventListener("DOMContentLoaded", async function(){
   document.querySelector("#sgfr").addEventListener("click", async function(){
     renderMap("data/locations/sgfr.json", markers)
   })
+  document.querySelector("#seveneleven").addEventListener("click", async function(){
+    renderMap("data/locations/seveneleven.json", markers)
+  })
+  document.querySelector("#cheers").addEventListener("click", async function(){
+    renderMap("data/locations/cheers.json", markers)
+  })
+  document.querySelector("#fairprice").addEventListener("click", async function(){
+    renderMap("data/locations/fairprice.json", markers)
+  })
+  document.querySelector("#giant").addEventListener("click", async function(){
+    renderMap("data/locations/giant.json", markers)
+  })
 })
